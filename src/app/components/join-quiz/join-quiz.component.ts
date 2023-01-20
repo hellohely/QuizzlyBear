8c5e376836cb642168d07ef56e3e05d045d37f51
Henrietta Lybeck <52459904+hellohely@users.noreply.github.com>
2023-01-20 21:59:39 +0100

    Sends username to server

--- src/app/components/join-quiz/join-quiz.component.ts
+++ src/app/components/join-quiz/join-quiz.component.ts
@@ -1,12 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { WebsocketService } from 'src/app/services/websocket.service';
 
 @Component({
   selector: 'app-join-quiz',
   templateUrl: './join-quiz.component.html',
 })
 export class JoinQuizComponent implements OnInit {
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private webSocketService: WebsocketService
+  ) {}
 
-  ngOnInit(): void {}
+  quizCode: string = '';
+  userName: string = '';
+
+  enterGame() {
+    console.log(this.userName);
+    this.webSocketService.emit('userName', this.userName);
+  }
+
+  ngOnInit(): void {
+    this.webSocketService.listen('testEvent').subscribe((data) => {
+      console.log(data);
+    });
+  }
 }
